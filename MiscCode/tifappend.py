f18a3346c9e65738df32415f181b5a989975eb3a
zchen <50221804+zhc008@users.noreply.github.com>
2023-05-18 20:08:21 -0400

    Add files via upload

--- MiscCode/tifappend.py
+++ MiscCode/tifappend.py
@@ -1,26 +1,31 @@
 import numpy as np
+import os
+import math
+import tifffile
+
 def tifappend(tiflink,image,tag,blocksize,imnum,filename):
-# function tiflink = tifappend(tiflink,image,tag,blocksize,imnum,filename)
 
-    # tiflink = tifappend(tiflink,image,tag,blocksize,imnum,filename)
-    #
-    # Function to write append the next scanned image to disk
-    #
-    # 2019 - Alex Song
+# tifappend(tiflink,image,tag,blocksize,imnum,filename)
+# return tiflink
+#
+# Function to write append the next scanned image to disk
+#
+# 2019 - Alex Song
 
-    if (np.mod(imnum,blocksize)==1 and imnum>1):
+    if imnum % blocksize == 1 and imnum > 1:
         tiflink.close()
-        clear tiflink
-        i = np.ceil(imnum/blocksize)
-        [path,name,_] = fileparts(filename)
-        filename2 = fullfile(path,sprintf([name '_#05d.tif'],i))
-        tiflink = Tiff(filename2,'w')
-        
+        del tiflink
+        i = int(math.ceil(imnum / blocksize))
+        path, name, _ = os.path.split(filename)
+        filename2 = os.path.join(path, f"{name}_{i:05d}.tif")
+        tiflink = tifffile.TiffWriter(filename2)
+
         tiflink.setTag(tag)
         tiflink.write(image)
-        tiflink.writeDirectory();  
+        tiflink.writeDirectory()
     else:
         tiflink.setTag(tag)
         tiflink.write(image)
-        tiflink.writeDirectory();  
+        tiflink.writeDirectory()
+
     return tiflink
--- MiscCode/tifread.py
+++ MiscCode/tifread.py
@@ -1,25 +1,28 @@
 import numpy as np
+import tifffile
+
 def tifread(fileTif,subImages=0):
-# function [finalImage, infoImage] = tifread(fileTif,subImages)
-    #Tifread - FileTif is a string specifying file location,
-    #subimages is len2 vector specifying start and end images (default all)
+# tifread(fileTif,subImages)
+# return finalImage, infoImage
+#Tifread - FileTif is a string specifying file location,
+#subimages is len2 vector specifying start and end images (default all)
 
-    infoImage=imfinfo(fileTif)
-    mImage=infoImage[0].Width
-    nImage=infoImage[0].Height
+    infoImage = tifffile.TiffFile(fileTif).pages
+    mImage = infoImage[0].width
+    nImage = infoImage[0].height
 
-    if subImages == 0:
+    if subImages is None:
         subImages = [1, len(infoImage)]
-        
 
-    tifLink = Tiff(fileTif, 'r')
-    tifLink.setDirectory(subImages[0])
-    im = tifLink.read()
-    finalImage=np.zeros((nImage,mImage,subImages[2]-subImages[1]+1),'like',im)
-    finalImage[:,:,0]=im
-    for i in range(subImages[1]-subImages[0]):
-        tifLink.setDirectory(i+subImages[1])
-        finalImage[:,:,i+1]=tifLink.read()
-    
+    tifLink = tifffile.TiffFile(fileTif)
+    im = tifLink.pages[subImages[0] - 1].asarray()
+    finalImage = np.zeros((nImage, mImage, subImages[1] - subImages[0] + 1), dtype=im.dtype)
+    finalImage[:, :, 0] = im
+
+    for i in range(subImages[1] - subImages[0]):
+        im = tifLink.pages[i + subImages[0]].asarray()
+        finalImage[:, :, i + 1] = im
+
     tifLink.close()
+
     return finalImage, infoImage
